8f7058842a9e1876733655a8b85908daa6d883a6
n-miyamoto <scotsdowneroad@gmail.com>
2017-10-15 11:51:09 +0900

    fix template

--- ATC/temp.cpp
+++ ATC/temp.cpp
@@ -1,16 +1,15 @@
-#include <stdio.h>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+//#include <stdio.h>
+//#include <iostream>
+//#include <algorithm>
 
 using namespace std;
 #define BUF (1000)
 
 int n;
-void input(void){
-	cin >> n;
-}
 
 int main(void){
-	input();
+	cin >> n;
+	
 	return 0;
 }
