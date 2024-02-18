dea890b92b043f91db3e242e7036c75c9c058774
n-miyamoto <scotsdowneroad@gmail.com>
2024-02-18 12:15:22 +0900

    refactor abc341 d

--- ATC/abc341/d.cpp
+++ ATC/abc341/d.cpp
@@ -1,58 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
 
-using namespace std;
-#define BUF (1000)
-#define ll long long 
+using ll = long long;
 
 ll lcm(ll a, ll b) {
-    return a / gcd(a, b) * b;
+    return a / std::gcd(a, b) * b;
 }
 
 int main() {
     ll n, m, k;
-	cin >> n >> m >> k;
+    std::cin >> n >> m >> k;
 
+    // Ensure n < m by swapping
+    if (n > m) {
+        std::swap(n, m);
+    }
 
-	//Guarantee n < m by swapping
-	if(n>m){
-		const auto tmp = n;
-		n=m;
-		m=tmp;
-	}
+    const ll l = lcm(n, m);
+    const ll countN = l / n - 1;
+    const ll countM = l / m - 1;
+    const ll countNM = countN + countM;
 
+    const ll blockNum = (k + countNM - 1) / countNM;
+    const ll kMod = (k + countNM - 1) % countNM + 1;
 
-	const auto l = lcm(n, m);
-	const auto count_n = l/n-1;
-	const auto count_m = l/m-1;
-	const auto count_nm= count_n+count_m;
+    for (int i = 1, j = 1, count = 1; i <= countN; i++, count++) {
+        ll current = n * i;
 
-	//cout << n << m << l << endl;
-	//cout << "n m nm " << count_n << count_m << count_nm << endl;
+        if (count == kMod) {
+            std::cout << (blockNum - 1) * l + current << std::endl;
+            return 0;
+        }
 
-	const auto block_num = (k+count_nm-1)/count_nm;
-	const auto k_mod = (k+count_nm-1)%count_nm + 1;
-	//cout << "block mod "  << block_num << k_mod << endl;
-
-	for(int i=1,j=1,count = 1; i <= count_n ; i++, count++){
-		auto cur = n*i;
-		//cout << "count cur " << count << cur << endl;
-
-		if(count==k_mod){
-			cout << (block_num-1) * l + cur << endl;
-			return 0;
-		}
-
-		
-		if(cur < j*m && j*m < (cur+n)){
-			count++;
-			cur = m*j;
-			j++;
-		}
-		if(count==k_mod){
-			cout << (block_num-1) * l + cur << endl;
-			return 0;
-		}
-	}
+        if (current < j * m && j * m < (current + n)) {
+            count++;
+            current = m * j;
+            j++;
+        }
+        if (count == kMod) {
+            std::cout << (blockNum - 1) * l + current << std::endl;
+            return 0;
+        }
+    }
 
     return 0;
 }
