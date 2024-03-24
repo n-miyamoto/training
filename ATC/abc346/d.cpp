d2847219b1d2b07f9ad0b52a3a698c9948db86bc
n-miyamoto <scotsdowneroad@gmail.com>
2024-03-24 13:50:43 +0900

    refactor abc346d

--- ATC/abc346/d.cpp
+++ ATC/abc346/d.cpp
@@ -1,9 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
-#define BUF (1000)
-#define ll long long 
-
+using ll = long long;
 
 int main(void){
 
@@ -16,45 +18,18 @@ int main(void){
 	vector<ll> costs(n);
 	for(auto& c: costs){ cin >> c;}
 	
-	vector<ll> prefix_sum0(n, 0);
-	vector<ll> prefix_sum1(n, 0);
+    vector<ll> costToChangeTo0(n + 1, 0), costToChangeTo1(n + 1, 0);
 
-	//initialize first elem
-	if(s[0]=='0'){
-		prefix_sum0[0] = 0;
-		prefix_sum1[0] = costs[0];
-	}else{
-		prefix_sum0[0] = costs[0];
-		prefix_sum1[0] = 0;
-	}
+    for (ll i = 0; i < n; ++i) {
+        costToChangeTo0[i + 1] = costToChangeTo0[i] + (((s[i] == '0') != (i%2==0)) ? costs[i] : 0);
+        costToChangeTo1[i + 1] = costToChangeTo1[i] + (((s[i] == '0') == (i%2==0)) ? costs[i] : 0);
+    }
 
-	for(ll i=1;i<n;i++){
-		if(i%2==0){ //even
-			if(s[i]=='0'){
-				prefix_sum0[i] = prefix_sum0[i-1];
-				prefix_sum1[i] = prefix_sum1[i-1] + costs[i];
-			}else{
-				prefix_sum0[i] = prefix_sum0[i-1] + costs[i];
-				prefix_sum1[i] = prefix_sum1[i-1];
-			}
-		}else{ //odd
-			if(s[i]=='1'){
-				prefix_sum0[i] = prefix_sum0[i-1];
-				prefix_sum1[i] = prefix_sum1[i-1] + costs[i];
-			}else{
-				prefix_sum0[i] = prefix_sum0[i-1] + costs[i];
-				prefix_sum1[i] = prefix_sum1[i-1];
-			}
-		}
-	}
-
-	const ll max = (ll)1000000001 * 200000;
-	ll ans = max;
+	ll ans = LLONG_MAX;
 	for(ll i=0;i<n-1;i++){
-		ll tmp0 = prefix_sum0[i] + prefix_sum1[n-1] - prefix_sum1[i];
-		ll tmp1 = prefix_sum1[i] + prefix_sum0[n-1] - prefix_sum0[i];
-
-		ans = min(ans, min(tmp0, tmp1));
+		ll cost0 = costToChangeTo0[i+1] + (costToChangeTo1[n] - costToChangeTo1[i+1]);
+		ll cost1 = costToChangeTo1[i+1] + (costToChangeTo0[n] - costToChangeTo0[i+1]);
+		ans = min(ans, min(cost0, cost1));
 	}
 
 	cout << ans << endl;
