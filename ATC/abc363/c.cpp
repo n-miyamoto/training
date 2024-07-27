823053893be89fea0e0929e122f3d10cb4a9bfcf
n-miyamoto <scotsdowneroad@gmail.com>
2024-07-27 10:23:32 +0900

    solve abc363c

---
+++ ATC/abc363/c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool isPalindrome(const std::string& str) {
+    int left = 0;
+    int right = str.size() - 1;
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+    return true;
+}
+
+bool hasPalindromeSubstring(const string& s, int m) {
+    int n = s.size();
+    for (int i = 0; i <= n - m; ++i) {
+        string substring = s.substr(i, m);
+        if (isPalindrome(substring)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(void){
+	int n, k;
+	cin >> n >> k;
+	string s;
+	cin >> s;
+
+	sort(s.begin(), s.end());
+
+	int ans=0;
+    do {
+		if(!hasPalindromeSubstring(s, k)) ans++;
+    } while (next_permutation(s.begin(), s.end()));
+
+	cout << ans << endl;
+
+	return 0;
+}
