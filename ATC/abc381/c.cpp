80e98530d4283b98b255b85cb8f7bb740cf383c3
n-miyamoto <scotsdowneroad@gmail.com>
2024-11-30 10:36:52 +0900

    refactor abc381c

--- ATC/abc381/c.cpp
+++ ATC/abc381/c.cpp
@@ -1,49 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
-using ll=long long;
 
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
 
-int main(void){
-	int n;
-	string s;
-	cin >> n;
-	cin >> s;
-	
-	vector<int> v;
-	s.push_back('0');
-	char stat = s[0];
-	int cnt=1;
-	for(int i=1;i<=n;i++){
-		if(stat == s[i]) cnt++;
-		else{
-			switch (stat)
-			{
-			case '1':
-				v.push_back(cnt);
-				break;
-			
-			case '2':
-				v.push_back(-cnt);
-				break;
+    vector<int> v;
+    s.push_back('0'); // Sentinel character to ensure processing the last segment
+    char current_char = s[0];
+    int count = 1;
 
-			case '/':
-				for(int j=0;j<cnt;j++) v.push_back(0);
-				break;
-			}
-			stat = s[i];
-			cnt = 1;
-		}
-	}
+    // Group consecutive characters and populate vector `v`
+    for (int i = 1; i <= n; ++i) {
+        if (s[i] == current_char) {
+            ++count;
+        } else {
+            if (current_char == '1') {
+                v.push_back(count);
+            } else if (current_char == '2') {
+                v.push_back(-count);
+            } else if (current_char == '/') {
+                v.insert(v.end(), count, 0); // Add `count` zeros to vector `v`
+            }
+            current_char = s[i];
+            count = 1;
+        }
+    }
 
-	//find 11/22
-	int ans = 1;
-	for(int i=1;i<v.size()-1; i++){
-		if(v[i-1] > 0 && v[i]==0 && v[i+1] < 0){
-			ans = max(ans, 2*min(v[i-1], -v[i+1])+1 );
-		}
-	}
+    // Compute the maximum length for the pattern "11/22"
+    int max_length = 1;
+    for (size_t i = 1; i + 1 < v.size(); ++i) {
+        if (v[i - 1] > 0 && v[i] == 0 && v[i + 1] < 0) {
+            max_length = max(max_length, 2 * min(v[i - 1], -v[i + 1]) + 1);
+        }
+    }
 
-	cout << ans << endl;
-	return 0;
+    cout << max_length << endl;
+    return 0;
 }
