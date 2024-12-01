9fa8d0c1e3f16e4a52a49f72430b904849460c11
n-miyamoto <scotsdowneroad@gmail.com>
2024-12-01 10:26:29 +0900

    refactor abc380c

--- ATC/abc380/c.cpp
+++ ATC/abc380/c.cpp
@@ -1,51 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
-using ll=long long;
 
+int main() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
 
-int main(void){
-	int n, k;
-	cin >> n >> k;
-	string s;
-	cin >> s;
+    // Encode the string into a vector of counts
+    s.push_back('/'); // Sentinel to mark the end
+    vector<int> v;
+    int count = 1;
 
-
-	//encode
-	s.push_back('/');
-	vector<int> v;
-	int count = 1;
     for (int i = 0; i < n; ++i) {
-        if (s[i] == s[i+1]) {
+        if (s[i] == s[i + 1]) {
             ++count;
         } else {
-            if (s[i]=='1') {
-                v.push_back(count);
-            }else{
-                v.push_back(-count);
-            } 
+            v.push_back(s[i] == '1' ? count : -count);
             count = 1;
         }
     }
 
-	//for(auto x : v) cout << x;
+    // Swap segments at the kth occurrence of '1'
+    for (int cnt = 0, i = 0; i < v.size(); ++i) {
+        if (v[i] > 0) ++cnt; // Count the '1' segments
+        if (cnt == k) {
+            if (i > 0) {
+                swap(v[i], v[i - 1]);
+            }
+            break;
+        }
+    }
 
-	for(int cnt = 0,i=0;i<v.size();i++){
-		if(v[i]>0) cnt++;
-		if(cnt==k){
-			int tmp = v[i];
-			v[i] = v[i-1];
-			v[i-1] = tmp;
-			break;
-		}
-	}
+    // Decode the vector back into the string
+    string result;
+    for (int x : v) {
+        if (x > 0) {
+            result.append(x, '1'); // Append `x` number of '1's
+        } else {
+            result.append(-x, '0'); // Append `-x` number of '0's
+        }
+    }
 
-	//for(auto x : v) cout << x;
-	
-	//decode;
-	for(auto x : v){
-		if(x>0) for(int i=0;i<x;i++) cout << '1';
-		else    for(int i=0;i<-x;i++) cout << '0';
-	}
-	cout << endl;
+    cout << result << endl;
+
+    return 0;
 }
